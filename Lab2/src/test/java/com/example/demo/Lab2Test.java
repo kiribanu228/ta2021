8fa412d9f7ea48bfa2159f48e4b208a0ed621efa
kiribanu228 <84561054+kiribanu228@users.noreply.github.com>
2021-06-29 08:58:32 +0300

    Add files via upload

---
+++ Lab2/src/test/java/com/example/demo/Lab2Test.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import com.example.demo.lab2.EquationSample;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author alaha
+ */
+@SpringBootTest
+public class Lab2Test {
+
+    @Autowired
+        EquationSample equationSample;
+    
+    @Test
+    void test1() {
+        assertEquals(3,equationSample.solve(1, 1, 1, 1, 1));
+        assertEquals(10,equationSample.solve(2, 2, 2, 2, 2));
+    }
+
+    
+
+ 
+}
